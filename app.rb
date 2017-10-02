032b9b5f24a422ff4bac772578585988fcef4d2b
Kristen Kulha and Megan Olson <megan@>
2017-10-02 10:00:57 -0700

    add layout and user login form

--- app.rb
+++ app.rb
@@ -8,3 +8,19 @@ enable :sessions
 before do
   @user = session[:id]
 end
+
+get "/" do
+  erb(:index)
+end
+
+post "/login" do
+  session[:id] = params.fetch("username")
+  password = params.fetch("password")
+  user = User.new({name: params["username"], score: 0})
+  if user.save
+    redirect back
+  else
+    @error_type = user
+    erb(:errors)
+  end
+end

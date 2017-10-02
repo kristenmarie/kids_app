e7629850f7ee75bafd8d3a15e03dab4bd174b32a
Kristen Kulha and Megan Olson <kristen@>
2017-10-02 09:40:28 -0700

    Add user class and base for app.rb.

---
+++ app.rb
@@ -0,0 +1,10 @@
+require("bundler/setup")
+require "pry"
+Bundler.require(:default)
+
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+enable :sessions
+
+before do
+  @user = session[:id]
+end
---
+++ lib/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+end
